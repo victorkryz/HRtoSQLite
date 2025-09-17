90415bd14c084c82f7629586dd2e63fde79745b7
Viktor Kryzhanivskyi <>
2025-09-17 18:59:53 +0300

    Update launchers

--- Launchers/startSingleTest.sh
+++ Launchers/startSingleTest.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 clear start
 export $(cat config.env | grep -v ^# | xargs)
-./../cmake-build-debug/HRtoSQLite_tests/g.tests/GTests_run --gtest_filter=$1
+# add libraries path:
+# export LD_LIBRARY_PATH="POCO libraries path:Oracle client libraries path":$LD_LIBRARY_PATH
+./../build/HRtoSQLite_tests/g.tests/GTests_run --gtest_filter=$1
