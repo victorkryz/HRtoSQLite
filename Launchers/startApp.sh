5b4c827899571ab68e9b1d373666775196702bba
Viktor Kryzhanivskyi <>
2025-10-08 17:11:17 +0300

    Update launchers

--- Launchers/startApp.sh
+++ Launchers/startApp.sh
@@ -2,4 +2,4 @@
 echo "Start HRtoSQLite..."
 # add libraries path:
 # export LD_LIBRARY_PATH="POCO libraries path:Oracle client libraries path":$LD_LIBRARY_PATH
-./../build/HRtoSQLite_run/HRtoSQLite_run //192.168.1.xx:1521/xepdb1 -N hr -P hr -S./../sql/hrSQLiteDDL.sql -O./../dbs/hr.db
+./../build/HRtoSQLite_run/HRtoSQLite_run -C//localhost:1521/FREEPDB1 -N hr -P hr -S./../sql/hrSQLiteDDL.sql -O./../dbs/hr.db
